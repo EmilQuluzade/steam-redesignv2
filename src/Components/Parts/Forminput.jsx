76e8ec734845e9ff421ea1baa789166efd384fbd
thedipi <dipiland777@gmail.com>
2023-07-13 22:29:42 +0400

    Signup done

--- src/Components/Parts/Forminput.jsx
+++ src/Components/Parts/Forminput.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import { useState } from "react";
 
-const Forminput = ({ placeholder , setUsername }) => {
+const FormInput = (props) => {
+  const [focused, setFocused] = useState(false);
+  const { label, errorMessage, onChange, id, ...inputProps } = props;
+
+  const handleFocus = (e) => {
+    setFocused(true);
+  };
+
   return (
     <div className="forminput">
-      {/* <label htmlFor="">{placeholder}</label> */}
-      <input placeholder={placeholder} onChange={e=>{setUsername(e.target.value)}} />
+      <label>{label}</label>
+      <input
+        {...inputProps}
+        onChange={onChange}
+        onBlur={handleFocus}
+        onFocus={() =>
+          inputProps.name === "confirmPassword" && setFocused(true)
+        }
+        focused={focused.toString()}
+      />
+      <span>{errorMessage}</span>
     </div>
   );
 };
 
-export default Forminput;
+export default FormInput;
